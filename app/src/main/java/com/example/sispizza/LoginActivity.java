40d87c0129588c8eb9ca98dc3e79ae0fd1e26f7a
Gutber Blanco <gutber.blanco@ucb.edu.bo>
2023-09-16 19:09:51 -0400

    Adding function for SHA256 in register and login

--- app/src/main/java/com/example/sispizza/LoginActivity.java
+++ app/src/main/java/com/example/sispizza/LoginActivity.java
@@ -2,12 +2,17 @@ package com.example.sispizza;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.sispizza.database.DatabaseHelper;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -35,12 +40,19 @@ public class LoginActivity extends AppCompatActivity {
                 String username = etUsername.getText().toString();
                 String password = etPassword.getText().toString();
 
-                if (dbHelper.authenticateUser(username, password)) {
+                // Convertir password ingresado
+                String passwordConverted = bin2hex(getHash(password));
+                Log.i("Password Hash Converted", passwordConverted);
+
+
+                if (dbHelper.authenticateUser(username, passwordConverted)) {
                     // Autenticación exitosa, redirigir a la siguiente pantalla.
+                    Log.i("Authentication", "Autenticacion exitosa dirigiendo a pantalla principal");
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
                 } else {
                     // Autenticación fallida, mostrar un mensaje de error.
+                    Log.i("Authentication", "Autenticacion fallida, las credenciales no coinciden");
                     Toast.makeText(LoginActivity.this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -52,10 +64,27 @@ public class LoginActivity extends AppCompatActivity {
         btnRegistrarse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 // Abrir la actividad de registro cuando se presiona el botón "Registrarse"
                 Intent intent = new Intent(LoginActivity.this, RegistroActivity.class);
                 startActivity(intent);
             }
         });
+
     }
+
+    public byte[] getHash(String password) {
+        MessageDigest digest=null;
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e1) {
+            e1.printStackTrace();
+        }
+        digest.reset();
+        return digest.digest(password.getBytes());
+    }
+    static String bin2hex(byte[] data) {
+        return String.format("%0" + (data.length*2) + "X", new BigInteger(1, data));
+    }
+
 }
--- app/src/main/java/com/example/sispizza/RegistroActivity.java
+++ app/src/main/java/com/example/sispizza/RegistroActivity.java
@@ -1,6 +1,7 @@
 package com.example.sispizza;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -8,6 +9,9 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.sispizza.database.DatabaseHelper;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
 public class RegistroActivity extends AppCompatActivity {
@@ -34,12 +38,19 @@ public class RegistroActivity extends AppCompatActivity {
                 String nuevoUsuario = etNuevoUsuario.getText().toString();
                 String nuevaContraseña = etNuevaContraseña.getText().toString();
 
+                // Aplicando Encriptacion SHA256
+                String passwordSha = bin2hex(getHash(nuevaContraseña));
+
+                Log.i("Password Hash saved",bin2hex(getHash(nuevaContraseña)));
+
                 // Agregar el nuevo usuario a la base de datos
-                long newRowId = dbHelper.addUser(nuevoUsuario, nuevaContraseña);
+                long newRowId = dbHelper.addUser(nuevoUsuario, passwordSha);
 
                 if (newRowId != -1) {
+
                     // Registro de usuario exitoso
                     Toast.makeText(RegistroActivity.this, "Usuario registrado con éxito", Toast.LENGTH_SHORT).show();
+
                     finish(); // Cierra la actividad de registro después de un registro exitoso
                 } else {
                     // Error al registrar el usuario
@@ -48,4 +59,19 @@ public class RegistroActivity extends AppCompatActivity {
             }
         });
     }
+
+    public byte[] getHash(String password) {
+        MessageDigest digest=null;
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e1) {
+            e1.printStackTrace();
+        }
+        digest.reset();
+        return digest.digest(password.getBytes());
+    }
+    static String bin2hex(byte[] data) {
+        return String.format("%0" + (data.length*2) + "X", new BigInteger(1, data));
+    }
+
 }
